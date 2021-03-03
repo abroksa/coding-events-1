129149c4ed56f050c76f4391329067a92ea5eb8b
abroksa <slipstitched@gmail.com>
2021-03-03 00:39:33 -0600

    Added unique ID

--- src/main/java/org/launchcode/codingevents/models/Event.java
+++ src/main/java/org/launchcode/codingevents/models/Event.java
@@ -4,9 +4,18 @@ public class Event {
     private String name;
     private String description;
 
+    private int id;
+    private static int nextId = 1;
+
     public Event(String name, String description) {
         this.name = name;
         this.description = description;
+        this.id = nextId;
+        nextId++;
+    }
+
+    public int getId() {
+        return id;
     }
 
     public String getName() {
